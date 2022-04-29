c35678ce9f5fcf09b949ad7a92ab31a6c76eb7c6
Akash Das <mfs.akash@gmail.com>
2022-04-29 20:48:41 +0530

    added a demo spring ATM application with custom events

--- eclipse-projects/spring/vscode-spring-project-demo/src/com/vscode/spring/project/demo/Application.java
+++ eclipse-projects/spring/vscode-spring-project-demo/src/com/vscode/spring/project/demo/Application.java
@@ -1,7 +1,14 @@
 package com.vscode.spring.project.demo;
 
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Spring application created with VSCode runs!");
+        AbstractApplicationContext context = new ClassPathXmlApplicationContext("application-context-config.xml");
+
+        System.out.println("Spring demo application created with VSCode");
+
+        context.close();
     }
 }
